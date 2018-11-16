5a82c87bccd28e73d7a0d39bc93a151862fbd549
LUCAS-UTF8 <LUCAS-UTF8@192.168.25.9>
2018-11-16 15:43:44 -0200

    Classe Player populada

--- Joguim/src/model/Player.java
+++ Joguim/src/model/Player.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Player {
-
+	private String name;
+	private int level;
+	private double exp;
+	private String vocation;
+	private int health;
+	private int mana;
+	//equipamentos
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public double getExp() {
+		return exp;
+	}
+	public void setExp(double exp) {
+		this.exp = exp;
+	}
+	public String getVocation() {
+		return vocation;
+	}
+	public void setVocation(String vocation) {
+		this.vocation = vocation;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getMana() {
+		return mana;
+	}
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
 }
